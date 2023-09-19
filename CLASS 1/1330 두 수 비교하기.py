d3116af01dedfeef0209c6af620cc8132c4aea0b
LapinMin <lapinuit966@gmail.com>
2023-09-19 11:24:49 +0900

    [0919] 1일차

---
+++ CLASS 1/1330 두 수 비교하기.py
@@ -0,0 +1,12 @@
+def compareAB(a, b):
+    if a > b:
+        print('>')
+    elif a == b:
+        print('==')
+    else:
+        print('<')
+        
+        
+a, b = map(int, input().split())
+
+compareAB(a, b)
---
+++ CLASS 1/2438 별 찍기 - 1.py
@@ -0,0 +1,9 @@
+a = int(input())
+
+def printStar(a):
+    for i in range(1, a+1):
+        for j in range(1, i+1):
+            print('*', end="")
+        print('')
+        
+printStar(a)
